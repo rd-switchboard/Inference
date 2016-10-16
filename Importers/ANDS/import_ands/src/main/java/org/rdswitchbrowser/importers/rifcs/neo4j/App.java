552b138a092a43ffaf7ceef77aaef404c6755ae9
Dima Kudriavcev <dmitrij@kudriavcev.info>
2016-10-16 17:40:12 +1100

    Added jackson to import_ands classpath (AWS for some reason missing it)

--- Importers/ANDS/import_ands/src/main/java/org/rdswitchbrowser/importers/rifcs/neo4j/App.java
+++ Importers/ANDS/import_ands/src/main/java/org/rdswitchbrowser/importers/rifcs/neo4j/App.java
@@ -121,7 +121,7 @@ public class App {
 	
 	private static void processS3Files(String bucket, String prefix, String neo4jFolder, 
 			String versionFolder, String source, CrosswalkRifCs.XmlType type, Templates template) throws Exception {
-        AmazonS3 s3client = new AmazonS3Client(new InstanceProfileCredentialsProvider());
+        AmazonS3 s3client = new AmazonS3Client(InstanceProfileCredentialsProvider.getInstance());
         
         CrosswalkRifCs crosswalk = new CrosswalkRifCs();
         crosswalk.setSource(source);
